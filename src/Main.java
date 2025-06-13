1081287e81ac041db28055e46b1989e273add995
mavlonovbaxtiyor <mavlonovbaxtiyor1983@gmail.com>
2025-06-13 10:18:05 +0500

    Real

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("25°C -> Fahrenheit: " + new FahrenheitConverter().convert(25));
         System.out.println("25°C -> Reaumur: " + new ReaumurConverter().convert(25));
 
-        Player f = new FootballPlayer("Zafar", 9, "Barcelona", 1, 2, 10);
+        Player f = new FootballPlayer("Zafar", 9, "Real Madrid", 1, 2, 10);
         Player v = new Setter("Sardor", 7, "UzVol", 30, 15);
         Player b = new PointGuard("Ali", 3, "NBA-UZ", 20, 7);
 
